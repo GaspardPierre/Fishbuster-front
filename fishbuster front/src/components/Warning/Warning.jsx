b74fe51fa7f2935b59afd0ed2e2becfa7a385c7f
GaspardPierre <pierredillard@gmail.com>
2023-07-10 15:39:11 +0200

    Warning.jsx

--- fishbuster front/src/components/Warning/Warning.jsx
+++ fishbuster front/src/components/Warning/Warning.jsx
@@ -4,12 +4,12 @@ import { AlertTriangle } from   'react-feather';
 export default function Warning() {
   return (
     <div className=' bg-red-800 '>
-        <h1 className='font-bold text-2xl'>Attention !!!</h1>
+        <h1 className='font-bold text-4xl text-center'>Attention !!!</h1>
         <AlertTriangle 
         color='black'
         size={200}
         />
-        <p>Ce site est certainement une tentative de phishing !!!</p>
+        <p className='text-center text-2xl '>Ce site est certainement une tentative de phishing !!!</p>
         </div>
   )
 }
