2d5dd27f26afc7557caca34e1e36af8dfb78ec8f
GaspardPierre <pierredillard@gmail.com>
2023-07-11 17:57:13 +0200

    Sail font

--- fishbuster front/src/components/Warning/Warning.jsx
+++ fishbuster front/src/components/Warning/Warning.jsx
@@ -5,7 +5,7 @@ export default function Warning() {
   return (
     <div className='w-full h-screen bg-red-800  '>
       <div className='w-full h-screen flex flex-col justify-center items-center'>
-        <h1 className='font-bold text-4xl text-center my-px'>Attention !!!</h1>
+        <h1 className='font-bold text-4xl md:text-5xl mdtext-center my-px font-lobster'>Attention !!!</h1>
       
         <AlertTriangle 
         className='text-center'
@@ -13,7 +13,7 @@ export default function Warning() {
         size={200}
         />
       
-        <p className='text-center text-2xl my-px '>Ce site est certainement une tentative de phishing !!!</p>
+        <p className='text-center text-2xl md:text-3xl my-px font-lobster '>Ce site est certainement une tentative de phishing !!!</p>
         </div>
         </div>
   )
--- fishbuster front/tailwind.config.js
+++ fishbuster front/tailwind.config.js
@@ -5,6 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sail': ['Sail', 'cursive'],
+      'roboto': ['Roboto', 'sans-serif'],
+      'montserrat': ['Montserrat', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
