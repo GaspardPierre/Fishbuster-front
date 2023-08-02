4ca58c6b45108df6b7f7fec0d9a106f6e9c70895
GaspardPierre <pierredillard@gmail.com>
2023-08-02 19:55:55 +0200

    navbar responsive

--- fishbuster front/src/components/NavBar/NavBar.jsx
+++ fishbuster front/src/components/NavBar/NavBar.jsx
@@ -5,22 +5,18 @@ import logo from "../../assets/Fishbuster.png";
 export default function NavBar() {
   return (
     <div className=" w-full h-28 bg-zinc-950">
-   
-<nav className="flex items-center justify-around flex-wrap ">
- 
-  
-        <div className="flex justify-evently w-full md:w-1/3">
-            {/* Search icon*/}
-        <Search color="white" size={24} />
-        {/* Search bar */}
-        <div className="w-full py-4 flex-col bg-slate-100 h-8 rounded-2xl  md:flex-row md:w-2/3 md:ml-6"></div>
-
+      <nav className="flex items-center justify-around flex-wrap ">
+        <div className=" mt-5 flex justify-evently w-5/6 md:w-1/3">
+          {/* Search icon*/}
+          <Search color="white" size={24} />
+          {/* Search bar */}
+          <div className=" w-full ml-3 py-4 flex-col bg-slate-100 h-8 rounded-2xl  md:flex-row md:w-2/3 md:ml-6"></div>
         </div>
-      
-        <span className="text-white text-lg  py-4">Fishbuster</span>
-        <span className="text-white text-lg  py-4">A propos</span>
-          </nav>
-      </div>
+        <div className=" w-5/6 flex items-center justify-between md:w-1/3">
+          <span className="text-white text-lg  py-4">Fishbuster</span>
+          <span className="text-white text-lg  py-4">A propos</span>
+        </div>
+      </nav>
+    </div>
   );
-  
 }
