70214a19a0b219bf1a25274f15516e7bf71c7a5b
GaspardPierre <pierredillard@gmail.com>
2023-09-19 15:12:07 +0200

    Title animation

--- fishbuster front/public/background.ts
+++ fishbuster front/public/background.ts
@@ -7,21 +7,28 @@ chrome.webNavigation.onCompleted.addListener(function(details) {
 
 function checkDomain(url) {
     // Extract domain of url
-   /*let domain = new URL(url).hostname;
+   let domain = new URL(url).hostname;
 
     // Send request to the backend
-    fetch('https://fisbuster.org/api/v1/malicious', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            token: 'my_test_token', 
-            domain: domain
-        })
-    })
+
+    try{
+        fetch('http://46.101.108.35:5000/api/v1/malicious', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                token: 'my_test_token', 
+                domain: domain
+            })
+
+    }
+   
+      
+    )
     .then(response => response.json())
     .then(data => {
+        console.log('Success:', data);
         if (data.level > 50) { 
         
             chrome.runtime.sendMessage({ type: "DOMAIN_CHECK_RESULT", isMalicious: true });
@@ -31,7 +38,10 @@ function checkDomain(url) {
     })
     .catch(error => {
         console.error('Erreur lors de la vérification du domaine:', error);
-    });*/
+    });
+}catch(error){
+    console.log(error);
+}
     setTimeout(() => {
         console.log('Hey check Domain is called');
         chrome.runtime.sendMessage({ type: "DOMAIN_CHECK_RESULT", isMalicious: false });
--- fishbuster front/src/popup/Home.tsx
+++ fishbuster front/src/popup/Home.tsx
@@ -1,32 +1,45 @@
-import React from "react";
+
 import fishbuster from"../../public/Fishbuster.png";
 import Bubble from "./Bubble";
 import {useSpring, animated } from 'react-spring';
 import '../../index.css';
-import logo from '../../public/logo.png ';
+
 
 
 function Home() {
+
+
   
-// animation of the fishbuster avatar
+// Animations
   const fishbusterProps = useSpring({
     from: { transform: 'translateX(100%)' },
     to: { transform: 'translateX(0%)' },
 
     config: { tension: 400, friction: 10},
-    delay: 2000, // control the delay of the animation
+    delay: 2000,
   });
+
+  const titleProps = useSpring({
+    from: { transform: 'scale(1)' },
+    to: { transform: 'scale(1.3)'},
+    config: { tension: 400, friction: 10 },
+    delay: 2300, 
+ 
+  });
+
+  
+
   return (
     <div className="relative h-screen overflow-hidden ">
 
      {Array.from({ length: 10 }).map((_, index) => (
       <Bubble key={index} />
     ))}
-   <div className="flex flex-col items-center justify-center h-screen  w-full space-y-4 bg-blue-500">
+   <div className="flex flex-col items-center justify-center h-screen  w-full space-y-4 bg-blue-400">
     <div className="py-4 flex justify-center ">
-    <h1 className="text-xl font-bold z-50 font-font-pixel">
-Fishbuster
-</h1>
+    <animated.h1 style={titleProps} className="text-xl text-gray-200 font-bold z-50 font-font-pixel">
+          Fishbuster
+        </animated.h1>
 
     </div>
   
@@ -40,13 +53,13 @@ Seuil d'activation 55%
 </p>
 
 <div className="flex justify-space-between font-font-pixel">
-<button className="bg-blue-700 hover:bg-blue-800 transition ease-in-out delay-300 text-sm px-1 text-gray-200 py-1 mx-1 rounded-md z-50 font-font-pixel">
+<button className="bg-blue-700 hover:bg-blue-800 transition ease-in-out delay-50 text-sm px-1 text-gray-200 py-1 mx-1 rounded-md z-50 font-font-pixel">
   Sites bloqués
 </button>
-<button className="bg-blue-700 hover:bg-blue-800 transition ease-in-out delay-300 text-sm px-1 mx-1 py-1 text-gray-200 rounded-md  z-50 font-font-pixel">
+<button className="bg-blue-700 hover:bg-blue-800 transition ease-in-out delay-50 text-sm px-1 mx-1 py-1 text-gray-200 rounded-md  z-50 font-font-pixel">
   Paramètres
 </button>
-<button className="bg-blue-700 hover:bg-blue-800 transition ease-in-out delay-300 text-sm px-1 py-1 mx-1 text-gray-200 rounded-md z-50 font-font-pixel">
+<button className="bg-blue-700 hover:bg-blue-800 transition ease-in-out delay-50 text-sm px-1 py-1 mx-1 text-gray-200 rounded-md z-50 font-font-pixel">
   Signaler ce site
 </button>
 </div>
